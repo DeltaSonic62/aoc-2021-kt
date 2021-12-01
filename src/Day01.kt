c2a210acc4cd258eb640e27c1ac6108b1fe96838
DeltaSonic62 <40446642+DeltaSonic62@users.noreply.github.com>
2021-12-01 01:34:50 -0500

    Day 1 Solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prevNum = input[0].toInt()
+        var numOfIncreases = 0
+
+        for (num in input) {
+            val i = num.toInt()
+
+            if (i > prevNum) {
+                numOfIncreases++
+            }
+
+            prevNum = i
+        }
+        return numOfIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val newList: MutableList<String> = mutableListOf()
+
+        for (i in 0..input.size - 3) {
+            newList.add((input[i].toInt() + input[i + 1].toInt() + input[i + 2].toInt()).toString())
+        }
+
+        return part1(newList)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
